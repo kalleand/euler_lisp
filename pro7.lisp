8e3c8fe5bb7a5bd70bcc8861008a37f378d86b30
Karl Johan Andreasson <kalleand@kth.se>
2013-07-23 22:51:24 +0200

    Two more solved problems.

---
+++ pro7.lisp
@@ -0,0 +1,18 @@
+; Project Euler problem 7
+;
+; Finds the 10 001st prime.
+(defun prime? (n lat)
+  (cond
+    ((null lat) t)
+    ((eq (mod n (car lat)) 0) nil)
+    (t (prime? n (cdr lat)))))
+
+(defun pro7 (n i lat)
+  (cond
+    ((prime? n lat) 
+     (cond
+       ((eq i 10001) n)
+       (t (pro7 (+ n 2) (+ i 1) (cons n lat)))))
+    (t (pro7 (+ n 2) i lat))))
+
+(pro7 3 2 '(2))
