fdb653514351138f351d877c7f0a7c587561a489
Karl Johan Andreasson <kalleand@kth.se>
2013-07-24 23:43:52 +0200

    Added problem 16.

---
+++ pro16.lisp
@@ -0,0 +1,12 @@
+#!/usr/bin/sbcl --script
+
+; Project Euler problem 16
+;
+; This one was really easy using LISP.
+; Summing the digits in a large number (2^1000).
+(defun pro16 (n)
+  (cond
+    ((= n 0) 0)
+    (t (+ (mod n 10) (pro16 (floor (/ n 10)))))))
+
+(print (pro16 (expt 2 1000)))
