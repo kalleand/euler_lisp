24a173c959b6acae3434acb5356f65dfa991c9e3
Karl Johan Andreasson <kalleand@kth.se>
2013-07-31 15:39:31 +0200

    Made problem 25 approach more understandable (starting from 1st number instead of 2nd).

--- pro25.lisp
+++ pro25.lisp
@@ -2,12 +2,10 @@
 ; Project Euler problem 25.
 ;
 ; Finds the first fibonacci number that has 1000 digits.
-;
-; We start at the second fibonacci number (1 ->1<- 2 3 5 8...).
 
 (defun pro25 (a b)
   (cond
-    ((>= (/ b (expt 10 999)) 1) 2)
+    ((>= (/ b (expt 10 999)) 1) 1)
     (t (1+ (pro25 b (+ a b))))))
 
-(print (pro25 1 1))
+(print (pro25 0 1))
